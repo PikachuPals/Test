3655e1297febb069628b3dc2dd1bbd623fc0134d
PikachuPals <PikachuPalsmail@gmail.com>
2019-10-11 17:46:46 +0100

    Revert "new print line"
    This reverts commit b7fac05f6c51004a27da1708006a2dfbc5d578be.

--- src/main/java/acrostic/Main.java
+++ src/main/java/acrostic/Main.java
@@ -15,6 +15,5 @@ public class Main{
     public static void main(String[] args) {
         String result = LibClass.acrostic(args);
         System.out.println("Result = " + result);
-        System.out.println("Please work.")
     }
 }
